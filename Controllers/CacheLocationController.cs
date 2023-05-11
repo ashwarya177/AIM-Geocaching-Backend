98b2edfd3a51a842f55ab98ddaa02c0e194c4482
aishwarya <srivastava.aishwarya177@gmail.com>
2023-05-11 16:44:58 -0700

    Updated Line Endings

--- Controllers/CacheLocationController.cs
+++ Controllers/CacheLocationController.cs
@@ -1,3 +1,36 @@
 ﻿using AIM_Geocaching_Backend.Models;
 using AIM_Geocaching_Backend.Services;
-using Microsoft.AspNetCore.Mvc;namespace AIM_Geocaching_Backend.Controllers;[ApiController][Route("api/[controller]")]public class CacheLocationController : ControllerBase{    private readonly ILogger<CacheLocationController> _logger;    public CacheLocationController(ILogger<CacheLocationController> logger)    {        _logger = logger;    }    [HttpGet(Name = "GetCacheLocation")]    public IActionResult Get()    {        var queryParams = HttpContext.Request.Query;        queryParams.TryGetValue("lat", out var latitude);        queryParams.TryGetValue("lon", out var longitude);        // Currently the code does not make use of user's current location params        // latitude, longitude provided in Query String.        // However, we can add logic based on the user's current location        // to show only nearby caches.                ParseGPX parse = new ParseGPX("aim.geocaches.gpx");        var cacheLocations = parse.LoadGPXWaypoints();        return Ok(cacheLocations);    }}
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIM_Geocaching_Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CacheLocationController : ControllerBase
+{
+    private readonly ILogger<CacheLocationController> _logger;
+
+    public CacheLocationController(ILogger<CacheLocationController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetCacheLocation")]
+    public IActionResult Get()
+    {
+        var queryParams = HttpContext.Request.Query;
+        queryParams.TryGetValue("lat", out var latitude);
+        queryParams.TryGetValue("lon", out var longitude);
+
+        // Currently the code does not make use of user's current location params
+        // latitude, longitude provided in Query String.
+        // However, we can add logic based on the user's current location
+        // to show only nearby caches.
+        
+        ParseGPX parse = new ParseGPX("aim.geocaches.gpx");
+        var cacheLocations = parse.LoadGPXWaypoints();
+
+        return Ok(cacheLocations);
+    }
+}
+
